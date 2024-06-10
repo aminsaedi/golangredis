07e5d12388386a28066744a02298e6cdfa3bf037
Amin Saedi <amin.di.sam@gmail.com>
2024-06-09 20:05:40 -0400

    CLI tests (cli-test-1717977940439)

--- app/server.go
+++ app/server.go
@@ -5,8 +5,16 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type DataItem struct {
+	value  string
+	expiry int
+}
+
+var storage = make(map[string]DataItem)
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -34,12 +42,34 @@ func toBulkString(input string) string {
 	return "$" + fmt.Sprint(len(input)) + "\r\n" + input + "\r\n"
 }
 
+func toSimpleString(input string) string {
+	return "+" + input + "\r\n"
+}
+
 func echo(value string) string {
 	return toBulkString(value)
 }
 
 func ping() string {
-	return "+PONG\r\n"
+	return toSimpleString("PONG")
+}
+
+func set(key, value string) string {
+	storage[key] = DataItem{
+		value:  value,
+		expiry: -1,
+	}
+	return toSimpleString("OK")
+}
+
+func get(key string) string {
+	fmt.Println("Getting " + key + "...")
+	item, ok := storage[key]
+	if ok {
+		return item.value
+	} else {
+		return toSimpleString("Error")
+	}
 }
 
 func handleRequest(conn net.Conn) {
@@ -53,24 +83,40 @@ func handleRequest(conn net.Conn) {
 
 		fmt.Println("In loop", tokens)
 
-		if len(tokens) < 3 {
-			continue
-		}
-		var result string
-		command := strings.ToUpper(tokens[2])
-		switch command {
-		case "ECHO":
-			if len(tokens) == 5 {
-				fmt.Println("Calling echo func", tokens)
-				result = echo(tokens[4])
+		if len(tokens) > 0 && strings.HasPrefix(tokens[0], "*") {
+			requiredItems, _ := strconv.Atoi(tokens[0][1:])
+			fmt.Println("Required to exec: ", requiredItems*2+1, " - Current: ", len(tokens))
+			if len(tokens) == requiredItems*2+1 {
+				// run command
+
+				var result string
+				command := strings.ToUpper(tokens[2])
+				switch command {
+				case "ECHO":
+					if len(tokens) != 5 {
+						continue
+					}
+					fmt.Println("Calling echo func", tokens)
+					result = echo(tokens[4])
+				case "PING":
+					result = ping()
+				case "SET":
+					result = set(tokens[4], tokens[6])
+				case "GET":
+					if len(tokens) != 4 {
+						continue
+					}
+					result = get(tokens[4])
+				}
+
+				fmt.Printf("Rresult is : %q\n", result)
+				conn.Write([]byte(result))
+
+				// reset tokens
 				tokens = make([]string, 0)
 			}
-		case "PING":
-			result = ping()
-			tokens = make([]string, 0)
 		}
-		fmt.Printf("Rresult is : %q\n", result)
-		conn.Write([]byte(result))
+
 	}
 	fmt.Println(tokens)
 }
