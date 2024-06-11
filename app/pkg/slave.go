e0300873478af5b65dbae8c99756229d1156cebf
Amin Saedi <amin.di.sam@gmail.com>
2024-06-11 09:35:47 -0400

    CLI tests (cli-test-1718112947257)

--- app/pkg/slave.go
+++ app/pkg/slave.go
@@ -15,11 +15,14 @@ func connectToMaster() {
 	}
 	defer conn.Close()
 
+	// read `empty.rdb` file and send it to the master
+
 	commands := []string{
 		internal.ToArray("PING"),
 		internal.ToArray("REPLCONF", "listening-port", fmt.Sprint(config.AppConfig.BindingPort)),
 		internal.ToArray("REPLCONF", "capa", "psync2"),
 		internal.ToArray("PSYNC", "?", "-1"),
+		internal.RDBFileToString("empty.rdb"),
 	}
 	for _, command := range commands {
 		conn.Write([]byte(command))
