e2e3de941179599fbc3ec77b7a6fdf3a00065665
Amin Saedi <amin.di.sam@gmail.com>
2024-06-11 04:32:57 -0400

    CLI tests (cli-test-1718094777364)

--- app/pkg/slave.go
+++ app/pkg/slave.go
@@ -1,8 +1,10 @@
 package pkg
 
 import (
+	"fmt"
 	"net"
 
+	"github.com/codecrafters-io/redis-starter-go/app/config"
 	"github.com/codecrafters-io/redis-starter-go/app/internal"
 )
 
@@ -19,6 +21,22 @@ func connectToMaster() {
 	// read RDB
 
 	// send PING
-	conn.Write([]byte(internal.ToArray("PING")))
+	// conn.Write([]byte(internal.ToArray("PING")))
 
+	// conn.Write([]byte(internal.ToArray("REPLCONF", "listening-port", fmt.Sprint(config.AppConfig.BindingPort))))
+	// conn.Write([]byte(internal.ToArray("REPLCONF", "capa", "psync2")))
+	commands := []string{
+		internal.ToArray("PING"),
+		internal.ToArray("REPLCONF", "listening-port", fmt.Sprint(config.AppConfig.BindingPort)),
+		internal.ToArray("REPLCONF", "capa", "psync2"),
+	}
+	for _, command := range commands {
+		fmt.Println("Sending command: ", command)
+		conn.Write([]byte(command))
+		// wait for reply
+		reply := make([]byte, 1024)
+		conn.Read(reply)
+		fmt.Println("Reply: ", string(reply))
+
+	}
 }
