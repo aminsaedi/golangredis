ef64f5953e5d40c69188beaff182caa415431094
Amin Saedi <amin.di.sam@gmail.com>
2024-06-11 03:54:50 -0400

    CLI tests (cli-test-1718092490633)

--- app/internal/parser.go
+++ app/internal/parser.go
@@ -2,8 +2,15 @@ package internal
 
 import "fmt"
 
-func ToBulkString(input string) string {
-	return "$" + fmt.Sprint(len(input)) + "\r\n" + input + "\r\n"
+func ToBulkString(input ...string) string {
+	// return "$" + fmt.Sprint(len(input)) + "\r\n" + input + "\r\n"
+	totalLength := 0
+	finalString := ""
+	for _, v := range input {
+		totalLength += len(v)
+		finalString += v + "\r\n"
+	}
+	return "$" + fmt.Sprint(totalLength) + "\r\n" + finalString
 }
 
 func ToSimpleString(input string) string {
