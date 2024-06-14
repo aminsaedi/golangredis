9aa204cfbecad921d260683baea8cee1a00333f4
Amin Saedi <amin.di.sam@gmail.com>
2024-06-13 23:21:09 -0400

    ACKs with commands

--- app/pkg/main.go
+++ app/pkg/main.go
@@ -69,6 +69,7 @@ func HandleRequestAsMaster(conn net.Conn, shouldSendResponse bool) {
 		matches := r.FindStringSubmatch(text)
 		if len(matches) > 0 {
 			// fmt.Println("Matched", text, matches[1])
+			tokens = make([]string, 0)
 			text = "*" + matches[1]
 		}
 
@@ -76,7 +77,7 @@ func HandleRequestAsMaster(conn net.Conn, shouldSendResponse bool) {
 
 		pp.Print(tokens)
 
-		if len(tokens) > 0 && strings.HasPrefix(tokens[0], "*") {
+		if len(tokens) > 0 && len(tokens[0]) > 1 && strings.HasPrefix(tokens[0], "*") {
 			requiredItems, _ := strconv.Atoi(tokens[0][1:])
 			requiredItems = requiredItems*2 + 1
 			if len(tokens) == requiredItems {
@@ -111,8 +112,8 @@ func HandleRequestAsMaster(conn net.Conn, shouldSendResponse bool) {
 					fmt.Println("Connected slave: ", conn.RemoteAddr().String())
 				}
 
-				fmt.Println("Command: ", command)
-				fmt.Println("Result: ", result)
+				totalTokenLength := len(strings.Join(tokens, "")) + (len(tokens) * 2)
+				c.PropogationStatus.TransferedBytes += totalTokenLength
 				if shouldSendResponse || command == "REPLCONF" {
 					conn.Write([]byte(result))
 				}
