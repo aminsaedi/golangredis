a4dc88670ea17defdde31b789fe4b0b1b94ef500
Amin Saedi <amin.di.sam@gmail.com>
2024-06-15 12:05:15 -0400

    CLI tests (cli-test-1718467515408)

--- app/internal/handler.go
+++ app/internal/handler.go
@@ -2,7 +2,9 @@ package internal
 
 import (
 	"fmt"
+	"net"
 	"os"
+	"regexp"
 	"strconv"
 	"time"
 
@@ -63,7 +65,10 @@ func Info(selection ...string) string {
 }
 
 func Replconf(args ...string) string {
-	fmt.Println("Replconf", args)
+	fmt.Println("Replconf: ", args)
+	if args[1] == "ACK" {
+		fmt.Println("ALLLLLLL")
+	}
 	if args[1] == "GETACK" {
 		return ToArray("REPLCONF", "ACK", strconv.Itoa(config.PropogationStatus.TransferedBytes))
 	}
@@ -83,6 +88,39 @@ func RDBFileToString(filePath string) string {
 }
 
 func Wait(args ...string) string {
-	fmt.Println("Wait", args)
-	return ToSimpleInt(c.AppConfig.ConnectedReplicasCount)
+	fmt.Println("Wait: ", args)
+	var waitTimeInMs, leastFullyPropogatedReplicasCount int
+	// leastFullyPropogatedReplicasCount = c.AppConfig.ConnectedReplicasCount
+
+	// var FullyPropogatedReplicaIds []string
+	// var mu sync.Mutex
+	// var wg sync.WaitGroup
+	ch := make(chan string, 50)
+
+	time.Sleep(time.Duration(100) * time.Millisecond)
+	for _, replica := range c.AppConfig.ConnectedReplicas {
+		// wg.Add(1)
+		go func(replica net.Conn) {
+			// defer wg.Done()
+			replica.Write([]byte(ToArray("REPLCONF", "GETACK", "*")))
+			buff := make([]byte, 64)
+			replica.Read(buff)
+			if regexp.MustCompile(`ACK`).Match(buff) {
+				fmt.Println("ACK Received from: ", replica.RemoteAddr().String())
+				slaveId := replica.RemoteAddr().String()
+				ch <- slaveId
+			}
+		}(replica)
+	}
+
+	// wg.Wait()
+	if len(args) == 4 {
+		waitTimeInMs, _ = strconv.Atoi(args[3])
+		leastFullyPropogatedReplicasCount, _ = strconv.Atoi(args[1])
+	}
+	if len(ch) < leastFullyPropogatedReplicasCount {
+		time.Sleep(time.Duration(waitTimeInMs) * time.Millisecond)
+	}
+	return ToSimpleInt(len(ch))
+	// return ToSimpleInt(100)
 }
