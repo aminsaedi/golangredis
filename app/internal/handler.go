9bb8e295a948328d31d3529be5bc011ed054112f
Amin Saedi <amin.di.sam@gmail.com>
2024-06-15 09:33:44 -0400

    CLI tests (cli-test-1718458424142)

--- app/internal/handler.go
+++ app/internal/handler.go
@@ -83,6 +83,14 @@ func RDBFileToString(filePath string) string {
 }
 
 func Wait(args ...string) string {
-	fmt.Println("Wait", args)
+	var waitTimeInMs, leastFullyPropogatedReplicasCount int
+	if len(args) == 4 {
+		waitTimeInMs, _ = strconv.Atoi(args[1])
+		leastFullyPropogatedReplicasCount, _ = strconv.Atoi(args[3])
+	}
+	if c.AppConfig.FullyPropogatedReplicasCount < leastFullyPropogatedReplicasCount {
+		time.Sleep(time.Duration(waitTimeInMs) * time.Millisecond)
+	}
+	fmt.Printf("Amin: %d\n", c.AppConfig.ConnectedReplicasCount)
 	return ToSimpleInt(c.AppConfig.ConnectedReplicasCount)
 }
