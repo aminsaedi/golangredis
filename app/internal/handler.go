9f6fa3e7ecaa254b527ed93cff811938a603f3c5
Amin Saedi <amin.di.sam@gmail.com>
2024-06-15 12:47:45 -0400

    CLI tests (cli-test-1718470065536)

--- app/internal/handler.go
+++ app/internal/handler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/codecrafters-io/redis-starter-go/app/config"
@@ -63,7 +64,10 @@ func Info(selection ...string) string {
 }
 
 func Replconf(args ...string) string {
-	fmt.Println("Replconf", args)
+	fmt.Println("Replconf: ", args)
+	if args[1] == "ACK" {
+		fmt.Println("ALLLLLLL")
+	}
 	if args[1] == "GETACK" {
 		return ToArray("REPLCONF", "ACK", strconv.Itoa(config.PropogationStatus.TransferedBytes))
 	}
@@ -82,7 +86,74 @@ func RDBFileToString(filePath string) string {
 	return "$" + fmt.Sprint(len(dat)) + "\r\n" + string(dat)
 }
 
+type Counter struct {
+	mu    sync.Mutex
+	count int
+}
+
+func (c *Counter) Increment() {
+	c.mu.Lock()
+	c.count++
+	c.mu.Unlock()
+}
+
+func (c *Counter) GetCount() int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.count
+}
+
 func Wait(args ...string) string {
-	fmt.Println("Wait", args)
-	return ToSimpleInt(c.AppConfig.ConnectedReplicasCount)
+	fmt.Println("Wait: ", args)
+	var waitTimeInMs, leastFullyPropogatedReplicasCount int
+
+	// var wg sync.WaitGroup
+	// ch := make(chan string, 50)
+	// var mu sync.Mutex
+	// var count int32
+
+	// counter := &Counter{}
+
+	// time.Sleep(time.Duration(50) * time.Millisecond)
+	// syncStatus := make(map[string]bool)
+	// for _, replica := range c.AppConfig.ConnectedReplicas {
+	// 	// wg.Add(1)
+	// 	syncStatus[replica.RemoteAddr().String()] = false
+	// 	go func(replica net.Conn) {
+	// 		// defer wg.Done()
+	// 		replica.Write([]byte(ToArray("REPLCONF", "GETACK", "*")))
+	// 		scanner := bufio.NewScanner(replica)
+	// 		reg := regexp.MustCompile(`ACK`)
+	// 		for scanner.Scan() {
+	// 			line := scanner.Text()
+	// 			fmt.Println("Got: ", line)
+	// 			if reg.MatchString(line) {
+	// 				// atomic.AddInt32(&count, 1)
+	// 				fmt.Println("Adding one for ", replica.RemoteAddr().String())
+	// 				mu.Lock()
+	// 				count++
+	// 				mu.Unlock()
+	// 				syncStatus[replica.RemoteAddr().String()] = true
+	// 				counter.Increment()
+	// 				break
+	// 			}
+	// 		}
+
+	// 	}(replica)
+	// }
+
+	// wg.Wait()
+	if len(args) == 4 {
+		waitTimeInMs, _ = strconv.Atoi(args[3])
+		leastFullyPropogatedReplicasCount, _ = strconv.Atoi(args[1])
+	}
+	if int(config.Counter.GetCount()) < leastFullyPropogatedReplicasCount {
+		time.Sleep(time.Duration(waitTimeInMs) * time.Millisecond)
+	}
+
+	time.Sleep(time.Duration(200) * time.Millisecond)
+	fmt.Println("Sending: ", config.Counter.GetCount())
+	// pp.Print(syncStatus)
+	return ToSimpleInt(int(config.Counter.GetCount()))
+	// return ToSimpleInt(100)
 }
