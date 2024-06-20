879426eee7d10f37096a2d5403382d44ed8e44ff
Amin Saedi <amin@fedora.tail635c9.ts.net>
2024-06-20 15:09:17 -0400

    CLI tests (cli-test-1718910557225)

---
+++ app/internal/rdb.go
@@ -0,0 +1,18 @@
+package internal
+
+import (
+	"fmt"
+	"os"
+
+	c "github.com/codecrafters-io/redis-starter-go/app/config"
+)
+
+func ReadRdbFile() {
+	path := c.AppConfig.Dir + "/" + c.AppConfig.Dbfilename
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return
+	}
+	fmt.Println("Reading RDB file: ", path)
+	fmt.Println(string(data))
+}
