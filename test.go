91828a8d79e964ff6044474a679d582d45504765
Amin Saedi <amin.di.sam@gmail.com>
2024-06-21 13:39:26 -0400

    CLI tests (cli-test-1718991566668)

--- test.go
+++ test.go
@@ -1,4 +1,4 @@
-package internal
+package main
 
 import (
 	"encoding/hex"
@@ -6,18 +6,27 @@ import (
 	"os"
 	"reflect"
 	"slices"
-
-	c "github.com/codecrafters-io/redis-starter-go/app/config"
 )
 
 var rdbMagicNumber = []byte{0x52, 0x45, 0x44, 0x49, 0x53}
 
-func ReadRdbFile() {
-	path := c.AppConfig.Dir + "/" + c.AppConfig.Dbfilename
-	byteData, err := os.ReadFile(path)
+func main() {
+	byteData, err := os.ReadFile("empty.rdb")
+
 	if err != nil {
-		return
+		panic(err)
 	}
+	// data := hex.EncodeToString(byteData)
+
+	// print file magic number
+
+	// print and split every one byte
+	// for i := 0; i < len(data); i += 2 {
+	// 	print(data[i : i+2])
+	// 	if i+2 < len(data) {
+	// 		print(" ")
+	// 	}
+	// }
 
 	if !reflect.DeepEqual(byteData[:len(rdbMagicNumber)], rdbMagicNumber) {
 		return
@@ -44,10 +53,16 @@ func ReadRdbFile() {
 			if ending == -1 {
 				ending = slices.Index(byteData[i+1:], 0xff)
 			}
-			dataBaseSection = append(dataBaseSection, byteData[i:i+ending+1])
+			dataBaseSection = append(dataBaseSection, byteData[i:i+ending])
 			i += ending
 		}
 	}
+
+	// fe00fb01000009626c756562657272790a73747261776265727279
+	dataBaseSection = [][]byte{{0xfe, 0x00, 0xfb, 0x01, 0x00, 0x00, 0x09, 0x62, 0x6c, 0x75, 0x62, 0x65, 0x72, 0x72, 0x79, 0x0a, 0x73, 0x74, 0x72, 0x61, 0x77, 0x62, 0x65, 0x72, 0x72, 0x79}}
+
+	// fe00fb0100000a737472617762657272790662616e616e61
+	// dataBaseSection = [][]byte{{0xfe, 0x00, 0xfb, 0x01, 0x00, 0x00, 0x0a, 0x73, 0x74, 0x72, 0x61, 0x77, 0x62, 0x65, 0x72, 0x72, 0x79, 0x06, 0x62, 0x61, 0x6e, 0x61, 0x6e, 0x61}}
 
 	fmt.Println("Meta data")
 	for _, meta := range metaData {
@@ -57,11 +72,16 @@ func ReadRdbFile() {
 	for _, data := range dataBaseSection {
 		fmt.Println(hex.EncodeToString(data))
 	}
+	for index, data := range dataBaseSection[0][6:] {
+		fmt.Print(hex.EncodeToString([]byte{data}))
+		fmt.Printf(" - %c - i:%d\n", data, index)
+	}
 
 	keyValues := make(map[string]string)
 
 	for i := 7; i < len(dataBaseSection[0]); i++ {
 		spliterIndex := slices.Index(dataBaseSection[0][i:], 0x0a)
+		fmt.Println("SpliterIndex", spliterIndex)
 		if spliterIndex == -1 {
 			break
 		}
@@ -76,5 +96,4 @@ func ReadRdbFile() {
 	}
 
 	fmt.Println(keyValues)
-
 }
