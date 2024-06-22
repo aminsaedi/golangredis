947dfc5235c3c6e8017c821b2c98065c4fe16384
Amin Saedi <amin.di.sam@gmail.com>
2024-06-22 00:54:35 -0400

    Read value with expiry #SM4

--- app/internal/rdb.go
+++ app/internal/rdb.go
@@ -1,11 +1,13 @@
 package internal
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"os"
 	"reflect"
 	"slices"
+	"time"
 
 	c "github.com/codecrafters-io/redis-starter-go/app/config"
 )
@@ -63,31 +65,77 @@ func ReadRdbFile() {
 		fmt.Printf(" - %c - i:%d\n", data, index)
 	}
 
-	i := 5
-	for {
-		fmt.Println("index", i)
+	// totalSize := int(dataBaseSection[0][3])
+	withExpiryItemSize := int(dataBaseSection[0][4])
 
-		keySize := int(dataBaseSection[0][i+1])
-		fmt.Println("Key size", keySize, " Key index", i+1)
-		valueSize := int(dataBaseSection[0][i+2+keySize])
-		fmt.Println("Value size", valueSize, " Value index", i+2+keySize)
+	if withExpiryItemSize == 0 {
+		i := 5
+		for {
 
-		key := dataBaseSection[0][i+2 : i+2+keySize]
-		value := dataBaseSection[0][i+3+keySize : i+3+keySize+valueSize]
+			keySize := int(dataBaseSection[0][i+1])
+			fmt.Println("Key size", keySize, " Key index", i+1)
+			valueSize := int(dataBaseSection[0][i+2+keySize])
+			fmt.Println("Value size", valueSize, " Value index", i+2+keySize)
 
-		fmt.Print("Key: ", string(key))
-		fmt.Println(" -- Value:", string(value))
+			key := dataBaseSection[0][i+2 : i+2+keySize]
+			value := dataBaseSection[0][i+3+keySize : i+3+keySize+valueSize]
 
-		SetStorageItem(string(key), DataItem{value: string(value)})
+			fmt.Print("Key: ", string(key))
+			fmt.Println(" -- Value:", string(value))
 
-		// find index of next 0x00
-		nextNull := slices.Index(dataBaseSection[0][i+3+keySize+valueSize:], 0x00)
-		if nextNull == -1 {
-			break
+			SetStorageItem(string(key), DataItem{value: string(value)})
+
+			// find index of next 0x00
+			nextNull := slices.Index(dataBaseSection[0][i+3+keySize+valueSize:], 0x00)
+			if nextNull == -1 {
+				break
+			}
+			i += 3 + keySize + valueSize + nextNull
+			if i >= len(dataBaseSection[0]) {
+				break
+			}
+
 		}
-		i += 3 + keySize + valueSize + nextNull
-		if i >= len(dataBaseSection[0]) {
-			break
+	} else {
+		i := 5
+		for {
+
+			fmt.Println("index", i)
+
+			firstByte := dataBaseSection[0][i]
+
+			var validTill time.Time
+			// expiry in Unix time milliseconds
+			if firstByte == 0xfc {
+				// next 8 bytes are expiry time
+				expiryTime := dataBaseSection[0][i+1 : i+9]
+				expiry := binary.LittleEndian.Uint64(expiryTime)
+				validTill = time.Unix(0, int64(expiry)*int64(time.Millisecond))
+				i += 9
+			}
+			keySize := int(dataBaseSection[0][i+1])
+			fmt.Println("Key size", keySize, " Key index", i+1)
+			valueSize := int(dataBaseSection[0][i+2+keySize])
+			fmt.Println("Value size", valueSize, " Value index", i+2+keySize)
+
+			key := dataBaseSection[0][i+2 : i+2+keySize]
+			value := dataBaseSection[0][i+3+keySize : i+3+keySize+valueSize]
+
+			fmt.Print("Key: ", string(key))
+			fmt.Println(" -- Value:", string(value))
+
+			SetStorageItem(string(key), DataItem{value: string(value), validTill: validTill})
+
+			// find index of next 0x00
+			nextNull := slices.Index(dataBaseSection[0][i+3+keySize+valueSize:], 0x00)
+			if nextNull == -1 {
+				break
+			}
+			i += 2 + keySize + valueSize + nextNull
+			if i >= len(dataBaseSection[0]) {
+				break
+			}
+
 		}
 	}
 
