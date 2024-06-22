f74599028eccb09c72c7d4bf3365c2f3e1186757
Amin Saedi <amin.di.sam@gmail.com>
2024-06-22 10:51:05 -0400

    CLI tests (cli-test-1719067865949)

--- app/internal/storage.go
+++ app/internal/storage.go
@@ -1,6 +1,10 @@
 package internal
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	t "github.com/codecrafters-io/redis-starter-go/app/tools"
@@ -16,7 +20,8 @@ type DataItem struct {
 }
 
 type StreamItem struct {
-	id string
+	key      string
+	entryIds []string
 }
 
 var plainStorage = make(map[string]DataItem)
@@ -29,15 +34,27 @@ func (di *DataItem) isValid() bool {
 	return di.validTill.After(time.Now())
 }
 
-func (si *StreamItem) addEntry(entryId string, dateItems []DataItem) {
+func (si *StreamItem) addEntry(entryId string, dataItems []DataItem) (ok bool, err error) {
 
-	for _, item := range dateItems {
+	// add entryId to the stream entryIds
+	si.entryIds = append(si.entryIds, entryId)
+
+	fmt.Println("entryIds__:", si.entryIds, "___", entryId)
+	isValid := isEntryIdsValid(si.key)
+
+	if !isValid {
+		fmt.Println("ERR The ID specified in XADD is equal or smaller than the target stream top item")
+		return false, errors.New("ERR The ID specified in XADD is equal or smaller than the target stream top item")
+	}
+
+	for _, item := range dataItems {
 		item.entryId = entryId
-		item.streamId = si.id
-		item.id = si.id + "_" + entryId
+		item.streamId = si.key
+		item.id = si.key + "_" + entryId
 
 		SetStorageItem(item.id, item)
 	}
+	return true, nil
 }
 
 func GetStorageItem(key string) (DataItem, bool) {
@@ -68,14 +85,14 @@ func GetAllKeys() []string {
 	return keys
 }
 
-func GetOrCreateStream(streamId string) StreamItem {
-	item, ok := streamStorage[streamId]
+func GetOrCreateStream(streamKey string) StreamItem {
+	item, ok := streamStorage[streamKey]
 	if !ok {
-		if streamId == "" {
-			streamId = t.GenerateRandomString()
+		if streamKey == "" {
+			streamKey = t.GenerateRandomString()
 		}
-		item = StreamItem{id: streamId}
-		streamStorage[streamId] = item
+		item = StreamItem{key: streamKey, entryIds: make([]string, 0)}
+		streamStorage[streamKey] = item
 	}
 	return item
 }
@@ -88,4 +105,54 @@ func IsStorageKeyValid(key string) bool {
 func IsStreamKeyValid(streamKey string) bool {
 	_, ok := streamStorage[streamKey]
 	return ok
+}
+
+func isEntryIdsValid(streamKey string) bool {
+	stream, ok := streamStorage[streamKey]
+	if !ok {
+		return false
+	}
+
+	fmt.Println("Ids:", stream.entryIds)
+
+	timestampMap := make(map[string][]int)
+
+	// Iterate over the array to split and group by timestamp
+	for _, item := range stream.entryIds {
+		parts := strings.Split(item, "-")
+		if len(parts) != 2 {
+			// Invalid format, skip this item or handle error
+			continue
+		}
+		timestamp := parts[0]
+		seqNum, err := strconv.Atoi(parts[1])
+		if err != nil {
+			// Invalid sequence number, skip this item or handle error
+			continue
+		}
+
+		// Add sequence number to the corresponding timestamp group
+		fmt.Println("Appending:", seqNum, " to ", timestamp)
+		timestampMap[timestamp] = append(timestampMap[timestamp], seqNum)
+	}
+
+	fmt.Println("timestampMap", timestampMap)
+
+	// Check if sequence numbers in each group are incremental
+	for _, seqNums := range timestampMap {
+		for i := 1; i < len(seqNums); i++ {
+			if seqNums[i] <= seqNums[i-1] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func AddEntryToStream(stream StreamItem, entryId string, dataItems []DataItem) (ok bool, err error) {
+	// si.addEntry(entryId, dataItems)
+	ok, err = stream.addEntry(entryId, dataItems)
+	streamStorage[stream.key] = stream
+	return ok, err
 }
