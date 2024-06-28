d504ce65562b6c92e4a786ea3ffa1ee7416cb50a
Amin Saedi <amin.di.sam@gmail.com>
2024-06-28 12:15:04 -0400

    CLI tests (cli-test-1719591304486)

--- app/internal/handler.go
+++ app/internal/handler.go
@@ -174,3 +174,16 @@ func Xadd(args ...string) string {
 	}
 	return ToBulkString(entryId)
 }
+
+func Xrange(args ...string) string {
+	streamKey := args[1]
+	start := args[3]
+	end := args[5]
+
+	stream := GetOrCreateStream(streamKey)
+	entries := GetStreamDataItemsInRange(stream, start, end)
+
+	fmt.Println("Entries: ", entries)
+
+	return ToSimpleError("Not implemented")
+}
