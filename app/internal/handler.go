88a385feff35b0666309650d947ec017ba7710ae
Amin Saedi <amin.di.sam@gmail.com>
2024-06-30 09:48:14 -0400

    CLI tests (cli-test-1719755294184)

--- app/internal/handler.go
+++ app/internal/handler.go
@@ -218,3 +218,47 @@ func Xrange(args ...string) string {
 
 	return finalResult
 }
+
+func Xread(args ...string) string {
+
+	fmt.Println("args:", args)
+	streamKey := args[3]
+	entryId := args[5]
+
+	stream := GetOrCreateStream(streamKey)
+
+	index := slices.Index(stream.entryIds, entryId)
+
+	if index == -1 {
+		return ToArray()
+	}
+
+	result := make([]string, 0)
+
+	result = append(result, ToSimpleString(streamKey))
+
+	entryIds := stream.entryIds[index+1:]
+
+	for _, entryId := range entryIds {
+		temp := make([]string, 0)
+		temp = append(temp, entryId)
+
+		temp2 := make([]string, 0)
+		item, ok := GetStorageItem(streamKey + "_" + entryId)
+		if ok {
+			temp2 = append(temp2, item.key)
+			temp2 = append(temp2, item.value)
+		}
+
+		temp2Str := ToArray(temp2...)
+		temp = append(temp, temp2Str)
+		result = append(result, ToArray(temp...))
+
+	}
+
+	finalResult := ToArray(result...)
+
+	fmt.Println("finalResult:", finalResult)
+
+	return finalResult
+}
